05841a897d86cfea09852c736797e13a0a538e32
حسام انور على غنيم <ghunaim@fg.gov.sa>
2025-08-10 10:25:08 +0300

    Dd

--- p1.py
+++ p1.py
@@ -9,7 +9,6 @@ def is_prime(n):
 
 def find_2nd_highest_num(arr):
     arr_copy = sorted(set(arr))
-    print(len(arr), len(arr_copy))
     return arr_copy[-2] if len(arr_copy) >= 2 else None
 
 print(find_2nd_highest_num([45,11,25,98,74,88,125,15,46,32,78,98,96,99,11,23]))
